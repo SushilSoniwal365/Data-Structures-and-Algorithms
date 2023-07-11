36bd536b5221188f852d5972ea3ba9e1f53dcb24
Sushil Soniwal <codersushil3@gmail.com>
2023-07-11 10:23:04 +0530

    1D Array Introduction

--- Array/Array_1D/src/T_01_Array_1D_Intro.java
+++ Array/Array_1D/src/T_01_Array_1D_Intro.java
@@ -11,18 +11,18 @@ public class T_01_Array_1D_Intro {
         System.out.println(arr[0] + " " + arr[1] + " " + arr[4]);
 
         // Todo:- 2nd way.
-//        Scanner in = new Scanner(System.in);
-//
-//        System.out.println("Enter the length of your desired array:- ");
-//        int len = in.nextInt();
-//
-//        int[] arr2 = new int[len];
-//
-//        System.out.println("Enter the elements your array till the entered length:- ");
-//        for (int idx = 0; idx < len; idx++) {
-//            arr2[idx] = in.nextInt();
-//        }
-//
-//        System.out.println("Your array is:- " + Arrays.toString(arr2));
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the length of your desired array:- ");
+        int len = in.nextInt();
+
+        int[] arr2 = new int[len];
+
+        System.out.println("Enter the elements your array till the entered length:- ");
+        for (int idx = 0; idx < len; idx++) {
+            arr2[idx] = in.nextInt();
+        }
+
+        System.out.println("Your array is:- " + Arrays.toString(arr2));
     }
 }
