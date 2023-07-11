c3a771c113ff56244cf56b35a5086df2cadf3cf9
Sushil Soniwal <codersushil3@gmail.com>
2023-07-11 10:09:11 +0530

    Array

--- Array/Array_1D/src/T_01_Array_1D_Intro.java
+++ Array/Array_1D/src/T_01_Array_1D_Intro.java
@@ -1,6 +1,10 @@
 public class T_01_Array_1D_Intro {
 
     public static void main(String[] args) {
-        
+        // 2 ways to declare and initialize an array.
+
+        // Todo:- 1st way.
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[4]);
     }
 }
