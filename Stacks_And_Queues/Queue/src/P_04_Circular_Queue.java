74a2e46e1614e58b9057beeb9b43520b05e77806
Sushil Soniwal <codersushil3@gmail.com>
2023-08-05 16:19:51 +0530

    Stacks & Queues

---
+++ Stacks_And_Queues/Queue/src/P_04_Circular_Queue.java
@@ -0,0 +1,63 @@
+public class P_04_Circular_Queue {
+
+    static class CircularQueue {
+        static int[] arr;
+        static int size;
+        static int front = -1, rear = -1;
+
+
+        public CircularQueue(int size) {
+            this.size = size;
+            arr = new int[size];
+        }
+
+        public boolean isEmpty() {
+            return (front == -1 && rear == -1);
+        }
+
+        public boolean isFull() {
+            return ((rear + 1) % size == front);
+        }
+
+        public void enqueue(int data) {
+            if (isFull()) {
+                return;
+            }
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public int dequeue() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int res = arr[front];
+            if (front == rear) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return res;
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularQueue q = new CircularQueue(5);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        System.out.println(q.dequeue());
+        q.enqueue(6);
+        System.out.println(q.dequeue());
+        q.enqueue(7);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.dequeue());
+        }
+    }
+}
